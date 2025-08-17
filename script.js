89ddc03c47cce75a62caa076836951404565e423
mi-nea <minacodes4ever@gmail.com>
2025-08-17 10:16:02 +0200

    added week 4 von kurs

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
-let weather = {
-  paris: {
-    temp: 19.7,
-    humidity: 80,
-  },
-  tokyo: {
-    temp: 17.3,
-    humidity: 50,
-  },
-  lisbon: {
-    temp: 30.2,
-    humidity: 20,
-  },
-  "san francisco": {
-    temp: 20.9,
-    humidity: 100,
-  },
-  oslo: {
-    temp: -5,
-    humidity: 20,
-  },
-};
-alert("Welcome to the weather app!");
-let city = prompt("Enter your city name").toLowerCase();
-if (city in weather) {
-  let temp = weather[city].temp;
-  let humidity = weather[city].humidity;
-  alert(
-    `The temperature in ${city} is currently ${temp} °C and the humidity is ${humidity}%`
-  );
-} else {
-  alert(
-    `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+in+${city}`
-  );
+function change(event) {
+  event.preventDefault();
+  alert("searching...");
+  let cityInputElement = document.querySelector("#search-input");
+  alert(cityInputElement.value);
+  //let cityName = cityInputElement.value;
+  let currentCity = document.querySelector("#current-city");
+  if (cityInputElement.value) {
+    currentCity.innerHTML = `${cityName}`;
+  } else {
+    currentCity.innerHTML = null;
+    alert("Please enter a city name.");
+  }
 }
+let searchInput = document.querySelector("#search-form");
+searchInput.addEventListener("submit", change);
+
+let currentDate = document.querySelector("#current-date");
+let currentTime = new Date();
+//let date = now.getDate();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[currentTime.getDay()]; //now.getday() = 0 - 6
+let hours = currentTime.getHours();
+let minutes = currentTime.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+currentDate.innerHTML = `${day} ${hours}:${minutes}, moderate rain`;
