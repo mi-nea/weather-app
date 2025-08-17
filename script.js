eb0f735ba94eab4520067085d0d081c683be50c7
mi-nea <minacodes4ever@gmail.com>
2025-08-17 10:25:12 +0200

    added week 5 von shecodes kurs

--- script.js
+++ script.js
@@ -1,39 +1,65 @@
-function change(event) {
-  event.preventDefault();
-  alert("searching...");
-  let cityInputElement = document.querySelector("#search-input");
-  alert(cityInputElement.value);
-  //let cityName = cityInputElement.value;
-  let currentCity = document.querySelector("#current-city");
-  if (cityInputElement.value) {
-    currentCity.innerHTML = `${cityName}`;
-  } else {
-    currentCity.innerHTML = null;
-    alert("Please enter a city name.");
-  }
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#current-temperature");
+  console.log(response);
+  let temperature = Math.round(response.data.temperature.current);
+  temperatureElement.innerHTML = temperature;
 }
-let searchInput = document.querySelector("#search-form");
-searchInput.addEventListener("submit", change);
 
-let currentDate = document.querySelector("#current-date");
-let currentTime = new Date();
-//let date = now.getDate();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[currentTime.getDay()]; //now.getday() = 0 - 6
-let hours = currentTime.getHours();
-let minutes = currentTime.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#current-city");
+  let apiKey = "7f7f5be77eb0oa403ta5130b5ff78ac4";
+  let city = searchInputElement.value;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+  cityElement.innerHTML = city;
 }
-if (hours < 10) {
-  hours = `0${hours}`;
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
 }
-currentDate.innerHTML = `${day} ${hours}:${minutes}, moderate rain`;
+
+// {
+// if (city= cityElement.value){
+//   console.log(response.data.temperature.current);
+//  let currentWeather = Math.round(response.data.temperature.current);
+//  let temperatureWeather = document.querySelector(
+//    ".current-temperature-value"
+//  );
+// temperatureWeather.innerHTML = `${currentWeather}`;
+// }
+
+//}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+let currentDateELement = document.querySelector("#current-date");
+let currentDate = new Date();
+
+currentDateELement.innerHTML = formatDate(currentDate);
